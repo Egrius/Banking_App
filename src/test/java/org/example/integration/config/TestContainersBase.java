c445cb43c78ce20aa8ac2aaa5b2f34687054f494
Egrius <egorminsk2006@gmail.com>
2026-03-30 22:27:48 +0300

    (dev): added testcontainers dependencies and config template

--- src/test/java/org/example/integration/config/TestContainersBase.java
+++ src/test/java/org/example/integration/config/TestContainersBase.java
@@ -1,5 +1,26 @@
 package org.example.integration.config;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+
+@Testcontainers
 public class TestContainersBase {
 
+    @Container
+    private static final PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:15-alpine")
+            .withDatabaseName("bank_db_test")
+            .withUsername("test")
+            .withPassword("test")
+            .withReuse(true);
+
+    @BeforeAll
+    static void init() {
+
+        System.setProperty("hibernate.connection.url", postgres.getJdbcUrl());
+        System.setProperty("hibernate.connection.username", postgres.getUsername());
+        System.setProperty("hibernate.connection.password", postgres.getPassword());
+        System.setProperty("hibernate.connection.driver_class", "org.postgresql.Driver");
+    }
 }
